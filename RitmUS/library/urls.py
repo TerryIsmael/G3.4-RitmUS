1770563a8ccb83a1a6cc8269c0c780cd371450f8
marmarsol4 <marmarsol4@alum.us.es>
2023-12-04 19:11:05 +0100

    Cambios de estilos y listado

--- RitmUS/library/urls.py
+++ RitmUS/library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, library_playlist_detail
 
 urlpatterns = [
     path('', index, name='index'),
-    #path('playlist/<int:pk>/', playlist_detail, name='playlist_detail'),
+    path('playlist/<int:pk>/', library_playlist_detail, name='library_playlist_detail'),
 ]
--- RitmUS/library/views.py
+++ RitmUS/library/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from base.models import Order, Subscription, Playlist, Song, User
-from datetime import datetime
+from base.models import Order, Subscription, Playlist, Song
+from datetime import datetime, timezone
 
 @login_required
 def index(request):
-    
+
     orders = Order.objects.filter(user=request.user.id)
     playlists_owned = []
     
     for order in orders:
-        suscriptions = Subscription.objects.filter(order=order.id)
-        playlists_owned = [suscription.playlist for suscription in suscriptions if suscription.end_date < datetime.now()]
+        subscriptions = Subscription.objects.filter(order=order)
+        playlists_owned = playlists_owned + [subscription.playlist for subscription in subscriptions if subscription.end_date > datetime.now(timezone.utc)]
  
     return render(request, 'index.html', {'playlists': playlists_owned})
 
-
-'''
-def playlist_detail(request, pk):
+@login_required
+def library_playlist_detail(request, pk):
     playlist = Playlist.objects.get(pk=pk)
+    subscription = Subscription.objects.filter(playlist=playlist, order__user= request.user, end_date__gt=datetime.now(timezone.utc)).first()
+    print("holo", subscription)
     songs = Song.objects.filter(playlist=playlist)
-    datos = {'playlist': playlist, 'songs': songs}
-    return render(request, 'playlist_details.html', datos)
-'''
+    return render(request, 'library_playlist_details.html', {'playlist': playlist, 'songs': songs, 'subscription': subscription})
