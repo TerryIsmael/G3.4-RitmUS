f24f1b257181daae455bbb882da947636242fb6f
marmarsol4 <marmarsol4@alum.us.es>
2023-12-04 17:37:20 +0100

    Cambios

--- RitmUS/library/urls.py
+++ RitmUS/library/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import index, playlist_detail
+from .views import index
 
 urlpatterns = [
-    path('/galeria', index, name='index'),
+    path('', index, name='index'),
     #path('playlist/<int:pk>/', playlist_detail, name='playlist_detail'),
 ]
--- RitmUS/library/views.py
+++ RitmUS/library/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from base.models import Order, Subscription, Playlist, Song, User
 from datetime import datetime
 
-
+@login_required
 def index(request):
-
-    orders = Order.objects.filter(user=request.user)
+    
+    orders = Order.objects.filter(user=request.user.id)
     playlists_owned = []
     
     for order in orders:
@@ -13,6 +14,8 @@ def index(request):
         playlists_owned = [suscription.playlist for suscription in suscriptions if suscription.end_date < datetime.now()]
  
     return render(request, 'index.html', {'playlists': playlists_owned})
+
+
 '''
 def playlist_detail(request, pk):
     playlist = Playlist.objects.get(pk=pk)
