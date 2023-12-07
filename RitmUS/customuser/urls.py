6ca67da056a3886af3fd03406e70cfc6922cd813
TerryIsmael <terryismael2@gmail.com>
2023-12-07 20:20:30 +0100

    Editar perfil

--- RitmUS/customuser/urls.py
+++ RitmUS/customuser/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, register,custom_login, create_incidence, list_incidences, playlist_detail, playlist_search
+from .views import home, register,custom_login, create_incidence, list_incidences,change_user,change_password
 
 
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('user/login/', custom_login, name='custom_login'),
     path('incidence/new/', create_incidence, name='create_incidence'),
     path('incidence/list/', list_incidences, name='list_incidences'),
-    path('playlist/<int:pk>/', playlist_detail, name='playlist_detail'),
-    path('playlist/search/', playlist_search, name='playlist_search'),
+    path('user/my_account', change_user, name='change_user'),
+    path('user/my_account/change_password', change_password, name='change_password'),
 ]
